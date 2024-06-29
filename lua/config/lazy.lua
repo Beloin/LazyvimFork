3dd8a9a6e6303e51f6acb5d339701eafdffdee13
Beloin Sena <beloin.rodrigues@gmail.com>
2024-06-29 01:35:55 -0300

    Added a few more pluguins Updated README.md

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,8 +18,12 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+    -- Extras
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
