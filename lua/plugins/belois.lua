a7bc9f5016a77c51fb1566445845547e8559f898
Beloin Rodrigues <beloinrodrigues.lds@grupomulti.com.br>
2024-08-14 14:16:08 -0300

    Add PERL configs

--- lua/plugins/belois.lua
+++ lua/plugins/belois.lua
@@ -68,25 +68,25 @@ return {
 
   { "Tastyep/structlog.nvim" },
 
-  {
-    "gorbit99/codewindow.nvim",
-    config = function()
-      local codewindow = require("codewindow")
-      codewindow.setup()
-      codewindow.apply_default_keybinds()
-    end,
-  },
+  -- Create a code window
+  -- {
+  --   "gorbit99/codewindow.nvim",
+  --   config = function()
+  --     local codewindow = require("codewindow")
+  --     codewindow.setup()
+  --     codewindow.apply_default_keybinds()
+  --   end,
+  -- },
 
   {
     "ziontee113/icon-picker.nvim",
     config = function()
       require("icon-picker").setup({ disable_legacy_commands = true })
 
-      local opts = { noremap = true, silent = true }
-
-      vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
-      vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
-      vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
+      -- local opts = { noremap = true, silent = true }
+      -- vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
+      -- vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
+      -- vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
     end,
   },
 
