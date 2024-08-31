3ccb2cea760b83ffe28bc2c3d585a71f1eaab5cd
Beloin Sena <beloin.rodrigues@gmail.com>
2024-08-31 00:55:21 -0300

    Added nvim-scissors

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -25,6 +25,10 @@ return {
     -- install jsregexp (optional!).
     build = "make install_jsregexp",
     dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.fn.stdpath("config") .. "/snippets/" })
+    end,
   },
 
   {
