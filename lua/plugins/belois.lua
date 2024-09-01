e2635c778da13a73f27c2dcdef312a2613c84a57
Beloin Sena <beloin.rodrigues@gmail.com>
2024-09-01 18:43:02 -0300

    Added better lsp signature

--- lua/plugins/belois.lua
+++ lua/plugins/belois.lua
@@ -143,4 +143,13 @@ return {
     end,
     lazy = false,
   },
+
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts)
+      require("lsp_signature").setup(opts)
+    end,
+  },
 }
