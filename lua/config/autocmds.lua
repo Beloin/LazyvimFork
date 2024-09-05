d7e528a29a6a7c0bb498e4f00f4a5e0ecda24ea2
Beloin Sena <beloin.rodrigues@gmail.com>
2024-09-05 20:43:13 -0300

    Removed "SessionManager", using persistence.nvim instead (built-in Lazyvim)

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,16 +12,13 @@ local autocmd = vim.api.nvim_create_autocmd
 autocmd("VimEnter", {
   group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
   callback = function()
-    LazyVim.on_load("neovim-session-manager", function()
       local args = vim.v.argv
       if #args >= 2 then
         if args[#args] == "." then
           print("neovim last arg was '.', running current dir session.")
-          vim.cmd(":SessionManager load_current_dir_session")
-          -- vim.cmd(":bufdo e")
+          require("persistence").load()
         end
       end
-    end)
   end,
   nested = true,
   -- once = true
