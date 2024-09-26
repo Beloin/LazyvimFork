f59b425bde79b1f20a180810caff4f5d3c91776d
Beloin Rodrigues <beloin.rodrigues@gmail.com>
2024-09-26 18:01:52 -0300

    Added CS skeleton
    Fixed LoremIpsum

--- lua/plugins/belois.lua
+++ lua/plugins/belois.lua
@@ -52,12 +52,9 @@ return {
 
   {
     "derektata/lorem.nvim",
+    event = "VeryLazy",
     config = function()
-      require("lorem").setup({
-        sentenceLength = "medium",
-        comma_chance = 0.2,
-        max_commas_per_sentence = 2,
-      })
+      require("lorem")
     end,
   },
 
