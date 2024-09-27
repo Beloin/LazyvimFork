50f391633ca7fbc4639e980ff3b7956997959a68
Beloin Rodrigues <beloin.rodrigues@gmail.com>
2024-09-27 11:57:30 -0300

    [skeleton] Added csharp skeleton
    [plugins] Added override for lualine

---
+++ lua/plugins/overrides.lua
@@ -0,0 +1,10 @@
+-- Overrides for defaults Lazyvim
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, "encoding")
+    end,
+  },
+}
