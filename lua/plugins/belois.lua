525098abcc15b50e37a758448d8405b63b50b53b
Beloin Sena <beloin.rodrigues@gmail.com>
2024-10-26 22:47:28 -0300

    [snippets] Added Java snippets
    - Add default colorscheme
    
    - Added goto-preview

--- lua/plugins/belois.lua
+++ lua/plugins/belois.lua
@@ -188,4 +188,10 @@ return {
       "folke/noice.nvim",
     },
   },
+
+  {
+    "rmagatti/goto-preview",
+    event = "BufEnter",
+    config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
+  },
 }
