05cb08611b550984abf0abf8412927e2103c4473
Beloin Sena <beloin.rodrigues@gmail.com>
2024-11-07 21:27:40 -0300

    [plugin] Removed old `scroll`
    - Added catppucin

--- lua/plugins/coloscheme.lua
+++ lua/plugins/coloscheme.lua
@@ -1,5 +1,6 @@
 return {
   { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
