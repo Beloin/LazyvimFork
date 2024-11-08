b8e2298ce7a706ee97c8c54b980148fae2b5b7f9
Beloin Sena <beloin.rodrigues@gmail.com>
2024-11-07 21:35:04 -0300

    [plugin] Added nightfox plugin

--- lua/plugins/coloscheme.lua
+++ lua/plugins/coloscheme.lua
@@ -1,6 +1,7 @@
 return {
   { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "EdenEast/nightfox.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
