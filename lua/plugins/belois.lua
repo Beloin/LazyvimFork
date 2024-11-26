d0a04a56e9113ecbfcc8027fff8de1c81b13ce2e
Beloin Sena <beloin.rodrigues@gmail.com>
2024-11-26 20:52:08 -0300

    [plugin] Added hlchunk.nvim to Highlight pieces of code

--- lua/plugins/belois.lua
+++ lua/plugins/belois.lua
@@ -230,4 +230,21 @@ return {
   {
     "ray-x/guihua.lua",
   },
+
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("hlchunk").setup({
+        chunk = {
+          enable = true,
+          -- ...
+        },
+        -- indent = {
+        --   enable = true,
+        --   -- ...
+        -- },
+      })
+    end,
+  },
 }
