2b5affd071c4290ed3aa20dd80405ffda98058c3
Beloin Sena <beloin.rodrigues@gmail.com>
2024-12-09 19:17:02 -0300

    [plugin] Removed BlameLine plugin in order to use gitsigns built-in Line Blame
    - Added ui-plugins file to add UI related plugins

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,3 +18,13 @@ autocmd("VimEnter", {
   nested = true,
   -- once = true
 })
+
+autocmd("BufEnter", {
+  callback = function()
+    local gs = require('gitsigns')
+    gs.toggle_current_line_blame(true)
+  end,
+  nested = true,
+  once = true
+})
+
--- lua/plugins/belois.lua
+++ lua/plugins/belois.lua
@@ -3,13 +3,6 @@
 return {
 
   {
-    "karb94/neoscroll.nvim",
-    config = function()
-      require("neoscroll").setup({})
-    end,
-  },
-
-  {
     "smoka7/multicursors.nvim",
     event = "VeryLazy",
     dependencies = {
@@ -27,24 +20,13 @@ return {
     },
   },
 
-  {
-    "braxtons12/blame_line.nvim",
-    config = function()
-      require("blame_line").setup({})
-    end,
-  },
-
+  -- Lazyvim has Gitsigns that already does this
   -- {
-  --   "petertriho/nvim-scrollbar",
+  --   "braxtons12/blame_line.nvim",
   --   config = function()
-  --     require("scrollbar").setup()
+  --     require("blame_line").setup({})
   --   end,
   -- },
-
-  {
-    "dstein64/nvim-scrollview",
-    config = function() end,
-  },
 
   {
     "derektata/lorem.nvim",
@@ -172,105 +154,6 @@ return {
     end,
     dependencies = {
       "folke/noice.nvim",
-    },
-  },
-
-  {
-    "rmagatti/goto-preview",
-    event = "BufEnter",
-    config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
-  },
-
-  {
-    "akinsho/git-conflict.nvim",
-    version = "*",
-    config = function()
-      require("git-conflict").setup({})
-
-      local wk = require("which-key")
-      wk.add({
-        { "<leader>gC", group = "Git Conflict" }, -- Adds a description to the "gt" group
-      })
-
-      vim.keymap.set("n", "<leader>gCo", "<cmd>GitConflictChooseOurs<cr>", { desc = "Conflit: Choose Ours" })
-      vim.keymap.set("n", "<leader>gCt", "<cmd>GitConflictChooseTheirs<cr>", { desc = "Conflit: Choose Theirs" })
-      vim.keymap.set("n", "<leader>gCb", "<cmd>GitConflictChooseBoth<cr>", { desc = "Conflit: Choose Both" })
-      vim.keymap.set("n", "<leader>gC0", "<cmd>GitConflictChooseNone<cr>", { desc = "Conflit: Choose None" })
-
-      return true
-    end,
-  },
-
-  {
-    "tiagovla/scope.nvim",
-    config = function()
-      require("scope").setup({})
-    end,
-  },
-
-  -- TODO: How to use only this instead of both?
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "VeryLazy",
-  --   opts = {},
-  --   config = function(_, opts)
-  --     require("lsp_signature").setup(opts)
-  --   end,
-  -- },
-
-  -- TODO: This gives too much text
-  {
-    "ray-x/navigator.lua",
-    dependencies = {
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-    },
-  },
-
-  {
-    "ray-x/guihua.lua",
-  },
-
-  {
-    "shellRaining/hlchunk.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("hlchunk").setup({
-        chunk = {
-          enable = true,
-          -- ...
-        },
-        -- indent = {
-        --   enable = true,
-        --   -- ...
-        -- },
-      })
-    end,
-  },
-
-  {
-    "sphamba/smear-cursor.nvim",
-
-    opts = {
-      -- Cursor color. Defaults to Cursor gui color
-      cursor_color = "#d3cdc3",
-
-      -- Background color. Defaults to Normal gui background color
-      normal_bg = "#282828",
-
-      -- Smear cursor when switching buffers
-      smear_between_buffers = true,
-
-      -- Smear cursor when moving within line or to neighbor lines
-      smear_between_neighbor_lines = true,
-
-      -- Use floating windows to display smears over wrapped lines or outside buffers.
-      -- May have performance issues with other plugins.
-      use_floating_windows = true,
-
-      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-      -- Smears will blend better on all backgrounds.
-      legacy_computing_symbols_support = false,
     },
   },
 
