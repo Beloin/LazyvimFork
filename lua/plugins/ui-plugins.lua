294ecbf7ddfe866577c641a0774d2c5f16366f51
Beloin Sena <beloin.rodrigues@gmail.com>
2024-12-15 11:56:17 -0300

    [plugin] Removed cmp-dap, since LazyVim is using blink-cmp.
    Also Removed neoscroll, Lazyvim is using default animations (from Snacks.nvim)

--- lua/plugins/ui-plugins.lua
+++ lua/plugins/ui-plugins.lua
@@ -2,13 +2,6 @@
 
 return {
 
-  {
-    "karb94/neoscroll.nvim",
-    config = function()
-      require("neoscroll").setup({})
-    end,
-  },
-
   -- {
   --   "petertriho/nvim-scrollbar",
   --   config = function()
