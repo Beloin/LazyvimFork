a1c06eb45afe44482790bdaadcf1174bf64b70ef
Beloin Sena <beloin.rodrigues@gmail.com>
2024-12-19 00:12:32 -0300

    [plugin](lang) Added FlutterTools

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -100,4 +100,14 @@ return {
       })
     end,
   },
+
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+  }
 }
