5fbbbcd5ec0de4df05d4965fe2c5ef86ef9f3fc1
Beloin Sena <beloin.rodrigues@gmail.com>
2025-01-09 21:46:55 -0300

    [feature] Added `CloseAllFloatingWindows` to config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,3 +15,17 @@ dap.listeners.before.event_exited["dapui_config"] = function()
   -- dapui.close({})
 end
 
+vim.api.nvim_create_user_command("CloseAllFloatingWindows", function(opts)
+  local closed_windows = {}
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local config = vim.api.nvim_win_get_config(win)
+    if config.relative ~= "" then -- is_floating_window?
+      vim.api.nvim_win_close(win, false) -- do not force
+      table.insert(closed_windows, win)
+    end
+  end
+  print(string.format("Closed %d windows: %s", #closed_windows, vim.inspect(closed_windows)))
+end, {
+  nargs = 0,
+  desc = "Remove all floating windows. Usually good when Plugins messes up",
+})
