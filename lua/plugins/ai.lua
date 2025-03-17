dba9c5780d92878471ac0c0c6c6b46e0a00ade0f
Beloin Sena <beloin.rodrigues@gmail.com>
2025-03-16 22:53:21 -0300

    [plugin] Using qwen as local default llm

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,21 +7,44 @@ return {
   config = function()
     require("codecompanion").setup({
       adapters = {
-        deepseek = function()
+        ollama = function()
           return require("codecompanion.adapters").extend("openai_compatible", {
             env = {
               url = "http://localhost:11434",
               api_key = "YOUR_API_KEY",
-              model = "deepseek-coder-v2:16b",
+              model = "qwen2.5-coder:14b",
             },
           })
         end,
       },
       strategies = {
-        chat = { adapter = "deepseek" },
-        inline = { adapter = "deepseek" },
-        agent = { adapter = "deepseek" },
+        chat = { adapter = "ollama" },
+        inline = { adapter = "ollama" },
+        agent = { adapter = "ollama" },
       },
     })
   end,
+
+  -- TODO: Implement llm.nvim
+  -- Giving error 500
+  -- {
+  --   "huggingface/llm.nvim",
+  --   config = function()
+  --     local llm = require("llm")
+  --     llm.setup({
+  --       backend = "ollama",
+  --       model = "qwen2.5-coder:14b",
+  --       url = "http://localhost:11434", -- llm-ls uses "/api/generate"
+  --       -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
+  --       request_body = {
+  --         -- Modelfile options for the model you use
+  --         options = {
+  --           temperature = 0.2,
+  --           top_p = 0.95,
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
+
 }
