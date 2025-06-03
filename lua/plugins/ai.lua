3fa8badfc951f69df1d8271de5c6b2ef2a2499db
Beloin Rodrigues <beloin.rodrigues@gmail.com>
2025-06-03 12:09:56 -0300

    [feature] Updating Avante AI plugin

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -55,9 +55,11 @@ return {
       -- add any opts here
       -- for example
       provider = "ollama",
-      ollama = {
-        endpoint = "http://localhost:11434",
-        model = "qwen2.5-coder:14b",
+      providers = {
+        ollama = {
+          endpoint = "http://100.83.175.60:11434",
+          model = "codellama:13b",
+        },
       },
       -- Too slow to use ollama provider, maybe create another faster AI to use as a provider
       -- ollama_fast = {
