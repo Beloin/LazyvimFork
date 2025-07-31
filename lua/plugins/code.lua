83f437c660f0fb8acae01efe56a839ea0d1945e0
Beloin Sena <beloin.rodrigues@gmail.com>
2025-07-31 17:10:09 -0300

    [fix] Removed Custom overrides

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -62,7 +62,7 @@ return {
   {
     "iabdelkareem/csharp.nvim",
     dependencies = {
-      "williamboman/mason.nvim", -- Required, automatically installs omnisharp
+      -- "williamboman/mason.nvim", -- Required, automatically installs omnisharp
       "mfussenegger/nvim-dap",
       "Tastyep/structlog.nvim", -- Optional, but highly recommended for debugging
     },
--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -7,4 +7,7 @@ return {
       table.insert(opts.sections.lualine_x, "encoding")
     end,
   },
+
+  -- { "mason-org/mason.nvim", version = "2.0.1" },
+  -- { "mason-org/mason-lspconfig.nvim", version = "2.0.1" },
 }
