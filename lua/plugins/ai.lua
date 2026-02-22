330cfcab3261d08f008ba4b3dd808b884ed5c887
Beloin Sena <beloin.rodrigues@gmail.com>
2026-02-21 22:09:12 -0300

    [plugin] Using again CodeCompanion, Avante is too Buggy

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -16,221 +16,248 @@ return {
   },
 
   -- Avante
-  {
-    "yetone/avante.nvim",
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    -- ⚠️ must add this setting! ! !
-    build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-      or "make",
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    ---@module 'avante'
-    ---@type avante.Config
-    opts = {
-      -- add any opts here
-      -- this file can contain specific instructions for your project
-      instructions_file = "avante.md",
-      auto_suggestions_provider = "ollama",
-      provider = "xai",
-      providers = {
-        deepseek = {
-          __inherited_from = "openai",
-          api_key_name = "DEEPSEEK_API_KEY",
-          endpoint = "https://api.deepseek.com",
-          model = "deepseek-chat",
-        },
-        ollama = {
-          model = "codegemma:7b",
-          endpoint = "http://localhost:11434",
-          is_env_set = true,
-          temperature = 0.1,
-          max_tokens = 2048,
-
-          -- IMPORTANT: enable FIM mode
-          use_fim = true,
-        },
-        deepseek_fast = {
-          __inherited_from = "openai",
-          endpoint = "http://localhost:11434",
-          model = "deepseek-coder:1.3b",
-        },
-        xai = {
-          api_key_name = "XAI_API_KEY",
-          model = "grok-code-fast-1",
-        },
-        openai = {
-          model = "gpt-5.1",
-        },
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          auth_type = "api", -- Set to "max" to sign in with Claude Pro/Max subscription
-          model = "claude-haiku-4-5-20251001",
-        },
-      },
-      shortcuts = {
-        {
-          name = "ask",
-          description = "Ask only promtp",
-          details = "Only ask wihtout making any changes to code.",
-          prompt = "Please answer me without making any changes to code.",
-        },
-      },
-    },
-    -- behaviour = {
-    --   auto_suggestions = true,
-    -- },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "stevearc/dressing.nvim", -- for input provider dressing
-      "folke/snacks.nvim", -- for input provider snacks
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-    -- system_prompt as function ensures LLM always has latest MCP server state
-    -- This is evaluated for every message, even in existing chats
-    system_prompt = function()
-      local hub = require("mcphub").get_hub_instance()
-      return hub and hub:get_active_servers_prompt() or ""
-    end,
-    -- Using function prevents requiring mcphub before it's loaded
-    custom_tools = function()
-      return {
-        require("mcphub.extensions.avante").mcp_tool(),
-      }
-    end,
-  },
-
-  -- Code Companion works quite well as a Sidekick
   -- {
-  --   "olimorris/codecompanion.nvim",
+  --   "yetone/avante.nvim",
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   -- ⚠️ must add this setting! ! !
+  --   build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+  --     or "make",
+  --   event = "VeryLazy",
+  --   version = false, -- Never set this value to "*"! Never!
+  --   ---@module 'avante'
+  --   ---@type avante.Config
+  --   opts = {
+  --     -- add any opts here
+  --     -- this file can contain specific instructions for your project
+  --     instructions_file = "avante.md",
+  --     auto_suggestions_provider = "ollama",
+  --     provider = "xai",
+  --     providers = {
+  --       deepseek = {
+  --         __inherited_from = "openai",
+  --         api_key_name = "DEEPSEEK_API_KEY",
+  --         endpoint = "https://api.deepseek.com",
+  --         model = "deepseek-chat",
+  --       },
+  --       ollama = {
+  --         model = "codegemma:7b",
+  --         endpoint = "http://localhost:11434",
+  --         is_env_set = true,
+  --         temperature = 0.1,
+  --         max_tokens = 2048,
+  --
+  --         -- IMPORTANT: enable FIM mode
+  --         use_fim = true,
+  --       },
+  --       deepseek_fast = {
+  --         __inherited_from = "openai",
+  --         endpoint = "http://localhost:11434",
+  --         model = "deepseek-coder:1.3b",
+  --       },
+  --       xai = {
+  --         api_key_name = "XAI_API_KEY",
+  --         -- model = "grok-code-fast-1",
+  --         model = "grok-4-1-fast-reasoning",
+  --       },
+  --       openai = {
+  --         model = "gpt-5.1",
+  --       },
+  --       claude = {
+  --         endpoint = "https://api.anthropic.com",
+  --         auth_type = "api", -- Set to "max" to sign in with Claude Pro/Max subscription
+  --         model = "claude-haiku-4-5-20251001",
+  --       },
+  --     },
+  --     shortcuts = {
+  --       {
+  --         name = "ask",
+  --         description = "Ask only promtp",
+  --         details = "Only ask wihtout making any changes to code.",
+  --         prompt = "Please answer me without making any changes to code.",
+  --       },
+  --     },
+  --   },
+  --   -- behaviour = {
+  --   --   auto_suggestions = true,
+  --   -- },
   --   dependencies = {
   --     "nvim-lua/plenary.nvim",
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "MeanderingProgrammer/render-markdown.nvim",
-  --     -- "OXY2DEV/markview.nvim",
-  --     "nvim-mini/mini.diff",
-  --     "ravitemer/mcphub.nvim",
-  --     "ravitemer/codecompanion-history.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+  --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+  --     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+  --     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+  --     "stevearc/dressing.nvim", -- for input provider dressing
+  --     "folke/snacks.nvim", -- for input provider snacks
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
   --   },
-  --   config = function()
-  --     require("codecompanion").setup({
-  --       prompt_library = {
-  --         markdown = {
-  --           dirs = {
-  --             vim.fn.getcwd() .. "/.prompts",
-  --             vim.fn.stdpath("config") .. "/promtps"
-  --           },
-  --         },
-  --       },
-  --
-  --       adapters = {
-  --         http = {
-  --           ollama = function()
-  --             return require("codecompanion.adapters").extend("ollama", {
-  --               env = {
-  --                 url = "http://localhost:11434",
-  --                 api_key = "OLLAMA_API_KEY",
-  --                 model = "qwen2.5-coder:14b",
-  --               },
-  --               headers = {
-  --                 ["Content-Type"] = "application/json",
-  --                 ["Authorization"] = "Bearer ${api_key}",
-  --               },
-  --               parameters = {
-  --                 sync = true,
-  --               },
-  --             })
-  --           end,
-  --           deepseek = function()
-  --             return require("codecompanion.adapters").extend("deepseek", {
-  --               env = {
-  --                 api_key = "DEEPSEEK_API_KEY",
-  --                 model = "deepseek-chat",
-  --               },
-  --               schema = {
-  --                 model = {
-  --                   default = "deepseek-chat",
-  --                 },
-  --               },
-  --             })
-  --           end,
-  --           grok = function()
-  --             return require("codecompanion.adapters").extend("openai_compatible", {
-  --               name = "grok",
-  --               url = "https://api.x.ai/v1/chat/completions",
-  --               env = {
-  --                 api_key = "XAI_API_KEY",
-  --               },
-  --               schema = {
-  --                 model = {
-  --                   default = "grok-code-fast-1",
-  --                 },
-  --                 presence_penalty = {
-  --                   hidden = true,
-  --                 },
-  --                 frequency_penalty = {
-  --                   hidden = true,
-  --                 },
-  --               },
-  --             })
-  --           end,
-  --         },
-  --       },
-  --       strategies = {
-  --         chat = { adapter = "grok" },
-  --         inline = { adapter = "grok" },
-  --         agent = { adapter = "grok" },
-  --       },
-  --       extensions = {
-  --         mcphub = {
-  --           callback = "mcphub.extensions.codecompanion",
-  --           opts = {
-  --             make_vars = true,
-  --             make_slash_commands = true,
-  --             show_result_in_chat = true,
-  --           },
-  --         },
-  --         history = {
-  --           enabled = true,
-  --           opts = {
-  --             dir_to_save = vim.fn.stdpath("data") .. "/codecompanion_chats.json",
-  --           },
-  --         },
-  --       },
-  --     })
+  --   -- system_prompt as function ensures LLM always has latest MCP server state
+  --   -- This is evaluated for every message, even in existing chats
+  --   system_prompt = function()
+  --     local hub = require("mcphub").get_hub_instance()
+  --     return hub and hub:get_active_servers_prompt() or ""
+  --   end,
+  --   -- Using function prevents requiring mcphub before it's loaded
+  --   custom_tools = function()
+  --     return {
+  --       require("mcphub.extensions.avante").mcp_tool(),
+  --     }
   --   end,
   -- },
+
+  -- Code Companion works quite well as a Sidekick
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "MeanderingProgrammer/render-markdown.nvim",
+      -- "OXY2DEV/markview.nvim",
+      "nvim-mini/mini.diff",
+      "ravitemer/mcphub.nvim",
+      "ravitemer/codecompanion-history.nvim",
+    },
+    config = function()
+      require("codecompanion").setup({
+        prompt_library = {
+          markdown = {
+            dirs = {
+              vim.fn.getcwd() .. "/.prompts",
+              vim.fn.stdpath("config") .. "/promtps",
+            },
+          },
+        },
+
+        adapters = {
+          http = {
+            openai = function()
+              return require("codecompanion.adapters").extend("openai", {
+                env = {
+                  api_key = "OPENAI_API_KEY",
+                  model = "gpt-5.1",
+                },
+                schema = {
+                  model = {
+                    default = "gpt-5.1",
+                  },
+                },
+              })
+            end,
+            claude = function()
+              return require("codecompanion.adapters").extend("anthropic", {
+                env = {
+                  api_key = "ANTHROPIC_API_KEY",
+                },
+                schema = {
+                  model = {
+                    default = "claude-haiku-4-5-20251001",
+                  },
+                },
+              })
+            end,
+            ollama = function()
+              return require("codecompanion.adapters").extend("ollama", {
+                env = {
+                  url = "http://localhost:11434",
+                  api_key = "OLLAMA_API_KEY",
+                  model = "qwen2.5-coder:14b",
+                },
+                headers = {
+                  ["Content-Type"] = "application/json",
+                  ["Authorization"] = "Bearer ${api_key}",
+                },
+                parameters = {
+                  sync = true,
+                },
+              })
+            end,
+            deepseek = function()
+              return require("codecompanion.adapters").extend("deepseek", {
+                env = {
+                  api_key = "DEEPSEEK_API_KEY",
+                  model = "deepseek-chat",
+                },
+                schema = {
+                  model = {
+                    default = "deepseek-chat",
+                  },
+                },
+              })
+            end,
+            grok = function()
+              return require("codecompanion.adapters").extend("openai_compatible", {
+                name = "grok",
+                url = "https://api.x.ai/v1/chat/completions",
+                env = {
+                  api_key = "XAI_API_KEY",
+                },
+                schema = {
+                  model = {
+                    -- default = "grok-code-fast-1",
+                    default = "grok-4-1-fast-reasoning",
+                  },
+                  presence_penalty = {
+                    hidden = true,
+                  },
+                  frequency_penalty = {
+                    hidden = true,
+                  },
+                },
+              })
+            end,
+          },
+        },
+        strategies = {
+          chat = { adapter = "grok" },
+          inline = { adapter = "grok" },
+          agent = { adapter = "grok" },
+        },
+        extensions = {
+          mcphub = {
+            callback = "mcphub.extensions.codecompanion",
+            opts = {
+              make_vars = true,
+              make_slash_commands = true,
+              show_result_in_chat = true,
+            },
+          },
+          history = {
+            enabled = true,
+            opts = {
+              dir_to_save = vim.fn.stdpath("data") .. "/codecompanion_chats.json",
+            },
+          },
+        },
+      })
+    end,
+  },
 
   -- UI for CodeCompanion
   {
